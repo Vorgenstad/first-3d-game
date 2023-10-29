b1f98c1809b10d3f98dd71bf0e95092b64ac1c4f
Vorgen <148902444+Vorgenstad@users.noreply.github.com>
2023-10-29 11:04:32 +0100

    Add squashing

--- scenes/player.gd
+++ scenes/player.gd
@@ -3,15 +3,29 @@ extends CharacterBody3D
 @export var speed = 14
 @export var fall_acceleration = 75
 @export var jump_impulse = 20
+@export var bounce_impulse = 16
 
 var target_velocity = Vector3.ZERO
 
 func _physics_process(delta):
-	var direction = calculate_direction()
+	calculate_input_velocity(delta)
 	
-	velocity = calculate_velocity(direction, delta)
+	calculate_collisions()
+	
+	velocity = target_velocity
 	
 	move_and_slide()
+
+func calculate_input_velocity(delta):
+	calculate_horizontal_velocity()
+	
+	calculate_vertical_velocity(delta)
+
+func calculate_horizontal_velocity():
+	var direction = calculate_direction()
+	
+	target_velocity.x = direction.x * speed
+	target_velocity.z = direction.z * speed
 
 func calculate_direction():
 	var direction = Vector3.ZERO
@@ -31,12 +45,21 @@ func calculate_direction():
 	
 	return direction
 
-func calculate_velocity(direction, delta):
-	var target_velocity = Vector3(direction.x * speed, 0, direction.z * speed)
-	target_velocity.z = direction.z * speed
-	
+func calculate_vertical_velocity(delta):
 	if not is_on_floor():
 		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
 	elif Input.is_action_just_pressed("jump"):
-	
-	return target_velocity
+		target_velocity.y = jump_impulse
+
+func calculate_collisions():
+	for index in range(get_slide_collision_count()):
+		var collision = get_slide_collision(index)
+
+		if collision.get_collider() == null:
+			continue
+
+		if collision.get_collider().is_in_group("Mob"):
+			var mob = collision.get_collider()
+			if Vector3.UP.dot(collision.get_normal()) > 0.1:
+				mob.squash()
+				target_velocity.y = bounce_impulse
